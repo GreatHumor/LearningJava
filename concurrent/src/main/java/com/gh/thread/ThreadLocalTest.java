0fabcd4c44ca12e0fbee79a164971d18a08751d3
罗子涵 <luozh@paraview.cn>
2021-09-12 21:49:35 +0800

    RF# ThreadLocal的hash算法

--- concurrent/src/main/java/com/gh/thread/ThreadLocalTest.java
+++ concurrent/src/main/java/com/gh/thread/ThreadLocalTest.java
@@ -14,6 +14,11 @@ public class ThreadLocalTest {
 
     private List<String> messages = new ArrayList<>();
 
+    /**
+     * threadLocalMap的hash算法所用的到递增值，每当创建一个ThreadLocal对象，
+     * 这个ThreadLocal.nextHashCode 这个值就会增长 0x61c88647
+     * 这个值很特殊，它是斐波那契数 也叫 黄金分割数。hash增量为 这个数字，带来的好处就是 hash 分布非常均匀。
+     */
     private static final int HASH_INCREMENT = 0x61c88647;
 
     public static final ThreadLocal<ThreadLocalTest> holder = ThreadLocal.withInitial(ThreadLocalTest::new);
@@ -29,11 +34,27 @@ public class ThreadLocalTest {
         return messages;
     }
 
+    /**
+     * threadLocal的基本使用
+     */
     @Test
     public void testThreadLocalGet() {
         ThreadLocalTest.add("今夜明珠色");
         System.out.println(holder.get().messages);
         ThreadLocalTest.clear();
+    }
+
+    /**
+     * 使用该数用于hash算法，能够让产生的哈希码分布得十分均匀
+     */
+    @Test
+    public void testHashIncr() {
+        int hashCode;
+        for (int i = 0; i < 16; i++) {
+            hashCode = i * HASH_INCREMENT + HASH_INCREMENT;
+            int bucket = hashCode & 15;
+            System.out.println(i + "在桶中的位置" + bucket);
+        }
     }
 
 
