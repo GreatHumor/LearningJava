c6ff7d3a10edf241e74c0d472fd1dc2c4134d5c6
luozh <silenceluo@icloud.com>
2021-09-28 23:17:17 +0800

    RF# 添加安全模块，des加密demo

---
+++ security/src/main/java/com/gh/encryption/DesTest.java
@@ -0,0 +1,78 @@
+package com.gh.encryption;
+
+import com.sun.org.apache.xml.internal.security.utils.Base64;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import javax.crypto.Cipher;
+
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * des加密
+ */
+public class DesTest {
+
+    // 算法
+    private static String algorithm = "DES";
+
+    private static Cipher cipher;
+
+    @Before
+    public void before() throws NoSuchPaddingException, NoSuchAlgorithmException {
+        cipher = Cipher.getInstance(algorithm);
+    }
+
+    /**
+     * 加密方法
+     * @param key 密钥 长度为8
+     * @param origin 原文
+     * @return 密文
+     * @throws Exception any
+     */
+    public static String encryptDes(String key, String origin) throws Exception {
+        SecretKeySpec secretKeySpec = new SecretKeySpec(key.getBytes(), algorithm);
+        cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
+        byte[] bytes = cipher.doFinal(origin.getBytes());
+        return Base64.encode(bytes);
+    }
+
+    /**
+     * 解密方法
+     * @param key 密钥 长度为8
+     * @param secret 密文
+     * @return 原文
+     * @throws Exception any
+     */
+    public static String decryptDes(String key, String secret) throws Exception {
+        SecretKeySpec secretKeySpec = new SecretKeySpec(key.getBytes(), algorithm);
+        cipher.init(Cipher.DECRYPT_MODE, secretKeySpec);
+        byte[] bytes = cipher.doFinal(Base64.decode(secret));
+        return new String(bytes);
+    }
+
+    @Test
+    public void testEncrypt() throws Exception {
+        // 原文
+        String input = "to do or not tod";
+        // 定义key, 如果使用DES加密，密钥必须是8位字节
+        String key = "12345678";
+        // 调用加密方法
+        System.out.println(encryptDes(key, input));
+    }
+
+    @Test
+    public void testDecrypt() throws Exception {
+        // 原文
+        String input = "to do or not tod";
+        // 定义key, 如果使用DES加密，密钥必须是8位字节
+        String key = "12345678";
+        String encryptDes = encryptDes(key, input);
+        String origin = decryptDes(key, encryptDes);
+        System.out.println(origin);
+    }
+}
